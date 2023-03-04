2dae851838c2bd1f7f568956054dbb0a84cd45e1
Abu Bakar <abubakarchaudhary@gmail.com>
2023-03-04 22:10:17 +0500

    MY Course Practice

---
+++ JAWA Script/1 Lec.js
@@ -0,0 +1,23 @@
+// [alert, var, document write,console log, prompt ] ye sb h is lec ma
+alert( "Hello World");
+// alert("Hii");
+
+var ab = "Hii! How are you " ;
+document.write( ab ) ; 
+
+
+
+
+var a = 5;
+var b = 5;
+var c = a+b;
+var d = a/b;
+var e = a*b;
+alert(`Your add Number is` + c ("<br>") `your Division no is` + d ("<br>")`your multiple no is` + e)
+console.log(c,d,e)
+
+
+var a=prompt("Enter your name")
+alert(a); 
+document.write(a);
+console.log(a);
---
+++ JAWA Script/2 Calculator.js
@@ -0,0 +1,18 @@
+var obm = prompt ("Enter Your Obtain Marks");
+var tm =  prompt ("Enter Your Total Marks");
+var per = (obm/tm)*100;
+alert(per);
+document.write(per);
+
+
+//      = Value Assign ki
+//      == Equal ha
+//      === Check Type i.e no. or string
+//      >= Greater than & Equal to
+//      <= Less than & Equal to
+
+
+//      Logics: 
+//      && AND Operator (a or b must b true)
+//      || OR Operator  (atleast one is true)
+//      ! Node Operator (yes h to no h, no hy to yes h)
