7912b6718a38500f4a2c0554cc0cb94849908913
Abu Bakar <abubakarchaudhary731@gmail.com>
2023-03-04 23:43:38 +0500

    Update 1 Lec.js

--- JAWA Script/1 Lec.js
+++ JAWA Script/1 Lec.js
@@ -1,11 +1,11 @@
 // [alert, var, document write,console log, prompt ] ye sb h is lec ma
+
+
 alert( "Hello World");
-// alert("Hii");
+alert("Hii");
 
 var ab = "Hii! How are you " ;
 document.write( ab ) ; 
-
-
 
 
 var a = 5;
@@ -13,11 +13,12 @@ var b = 5;
 var c = a+b;
 var d = a/b;
 var e = a*b;
-alert(`Your add Number is` + c ("<br>") `your Division no is` + d ("<br>")`your multiple no is` + e)
+document.write(`Your add Number is ${c} <br> your Division no is  ${d} <br>  your multiple no is ${e} <br>` )
 console.log(c,d,e)
 
 
-var a=prompt("Enter your name")
-alert(a); 
-document.write(a);
-console.log(a);
+
+var n=prompt("Enter your name");
+alert(`hii ${n}`); 
+document.write(n);
+console.log(n);
