7196bd7f02f51574f483656cb4394de437701cdd
Abu Bakar <abubakarchaudhary731@gmail.com>
2023-03-13 21:34:49 +0500

    Add

--- JAWA Script/1.1 Array.js
+++ JAWA Script/1.1 Array.js
@@ -1,5 +1,5 @@
 
-let arr =  [14,true,"ahmed", null];
+var arr =  [14,true,"ahmed", null];
 document.write(`${arr[1]} <br>`);
 
 
@@ -7,4 +7,9 @@ document.write(`${arr[1]} <br>`);
 
 // 2D Array 
 var Darr =  [14,true,"ahmed",[5,9,"ali",[6,"umer",45]],null];
-document.write(Darr[3][3][1]);
+document.write(`${Darr[3][3][1]} <br>`);
+
+
+var a = [3,"ahmed","ali"];
+const ans = [rollno,name,fathername]=a;
+document.write(name,' ' + fathername)
